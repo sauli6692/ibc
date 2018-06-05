f527af4fa43fe63552a9f68e0b869379bcf99ea2
Sauli Rodriguez <sauli@e-libro.com>
2018-06-04 23:42:52 -0600

    Added min admin

--- min/admin.py
+++ min/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Ministry,
+    MinistryObjective,
+)
+
+
+class MinistryObjectiveInline(admin.TabularInline):
+    model = MinistryObjective
+    extra = 0
+
+
+@admin.register(Ministry)
+class MinistryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description',)
+    list_filter = ('name', 'description')
+    list_display_links = ['pk', 'name']
+    ordering = ('pk', 'name',)
+    search_fields = ('name', 'description',)
+    inlines = [MinistryObjectiveInline]
